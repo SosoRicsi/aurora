fa2c4a2f44ec36dc555c7e509030e964c4b6492e
Jarkó Richárd <jarkoricsi16@gmail.com>
2025-05-31 15:57:39 +0200

    Remove trash

--- web/routes.php
+++ web/routes.php
@@ -6,7 +6,6 @@ use Radiant\Session\Session;
 use Aurora\http\middlewares\StartSession;
 use Aurora\http\Models\User;
 
-Route::get('/a', function () {
-	print "<pre>";
-	print_r(Session::flashed('alma'));
+Route::get('/test', function () {
+	print "test";
 });
